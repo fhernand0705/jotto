53d15a32a40a6475806234b1933209afdf5b1a3a
Freddy Hernandez <freddy.hernandez@fandm.edu>
2020-10-02 13:28:07 -0400

    added guessedWords component + tests

---
+++ src/GuessedWords/GuessWords.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import PropTypes from 'prop-types'; 
+
+const GuessedWords = ({ guessedWords }) => {
+    let contents; 
+    if (!guessedWords.length) {
+        contents = (
+            <span data-test="guess-instructions">
+                Try to guess the secret!
+            </span>
+        );
+    } else {
+        const guessedWordsRows = guessedWords.map(({guessedWord, letterMatchCount}, i) => (
+        <tr data-test="guessed-word" key={i}>
+            <td>{guessedWord}</td>
+            <td>{letterMatchCount}</td>
+        </tr>
+        ))
+
+        contents = (
+            <div data-test="guessed-words">
+                <h3>Guessed Words</h3>
+                <table>
+                    <thead>
+                        <tr><th>Guessed</th><th>Matching Letters</th></tr>
+                    </thead>
+                    <tbody>
+                        {guessedWordsRows}
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+    return (
+        <div data-test="component-guessed-word">
+            { contents }
+        </div>
+    )
+};
+
+export default GuessedWords;
+
+GuessedWords.propTypes = {
+    guessedWords: PropTypes.arrayOf(
+        PropTypes.shape({
+            guessedWord: PropTypes.string.isRequired,
+            letterMatchCount: PropTypes.number.isRequired
+        })
+    ).isRequired
+};
---
+++ src/GuessedWords/GuessWords.test.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import { shallow } from 'enzyme'; 
+
+import { checkProps, findByTestAttr } from '../../test/testUtils';
+import GuessedWords from './GuessWords';
+
+const defaultProps = {
+    guessedWords: [{ guessedWord: 'train', letterMatchCount: 3 }],
+};
+
+const setup = (props={}) => {
+    const setupProps = { ...defaultProps, ...props}
+    return shallow(<GuessedWords {...setupProps}/>)
+}
+
+test("does not throw a warning with expected props", () => {
+    checkProps(GuessedWords, defaultProps)
+})
+
+describe("if there are no words guessed", () => {
+    let wrapper; 
+
+    beforeEach(() => {
+        wrapper = setup({ guessedWords: [] });
+    })
+
+    test("renders without any errors", () => {
+        const component = findByTestAttr(wrapper, 'component-guessed-word');
+        expect(component.length).toBe(1)
+    })
+
+    test("renders instructions to guess a word", () => {
+        const instructions = findByTestAttr(wrapper, 'guess-instructions');
+        expect(instructions.text().length).not.toBe(0);
+    })
+})
+
+describe("if there are words guessed", () => {
+    let wrapper;
+    const guessedWords = [
+        { guessedWord: 'lebron', letterMatchCount: 3},
+        { guessedWord: 'davis', letterMatchCount: 1},
+        { guessedWord: 'rondo', letterMatchCount: 2},
+    ];
+
+    beforeEach(() => {
+        wrapper = setup({ guessedWords })
+    });
+
+    test("renders without any errors", () => {
+        const component = findByTestAttr(wrapper, 'component-guessed-word');
+        expect(component.length).toBe(1)
+    });
+
+    test("renders 'guessed words' section", () => {
+        const guessedWordsNode = findByTestAttr(wrapper, 'guessed-words');
+        expect(guessedWordsNode.length).toBe(1);
+    })
+
+    test("renders correct number of guessed words", () => {
+        const guessedWordsNodes = findByTestAttr(wrapper, 'guessed-word');
+        expect(guessedWordsNodes.length).toBe(guessedWords.length)
+    })
+})
